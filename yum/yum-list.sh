6a6d5a801d4a973d9a70d3b0481c7b865707facc
root <root@master.lan>
2023-08-09 20:10:23 +0800

    update

---
+++ yum/yum-list.sh
@@ -0,0 +1,1 @@
+yum install -y wget net-tools nfs-utils lrzsz gcc gcc-c++ make cmake libxml2-devel openssl-devel curl curl devel unzip sudo ntp libaio-devel wget vim tmux ncurses-devel autoconf automake zlib-devel python-devel epel-release openssh-server socat ipvsadm conntrack
