bdf6252da73351f84c20a7db62314f412767bb43
caapap <37110214+caapap@users.noreply.github.com>
2023-08-22 15:35:13 +0800

    Update k8s_init.sh

--- scripts/k8s_init.sh
+++ scripts/k8s_init.sh
@@ -5,14 +5,36 @@
 
 IP=ip -4 -o route get 10/8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'
 
+cat <<EOF > ~/.vimrc
+syntax on
+set tabstop=2
+set shiftwidth=2
+set expandtab
+set ai
+"set number
+set hlsearch
+set ruler
+highlight Comment ctermfg=green
+map s <nop>
+map S :<CR>
+map Q :q<CR>
+map R :source $MYVIMRC<CR>
+EOF
+
+cat << EOF >> ~/.bashrc
+export BASH_SILENCE_DEPRECATION_WARNING=1
+export PS1='\u@\h:\[\e[01;32m\]\w\[\e[0m\]\$ '
+export EDITOR='vim'
+export CLICOLOR=1
+alias ls='ls --color'
+EOF
+source ~/.bashrc
+
 ## aliyun source repo
-mv -f /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
-wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-yum clean all
-yum makecache
-yum -y update
+yum install -y wget gcc git jq net-tools yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
 cat <<EOF > ~/.vimrc
 syntax on
