d0f7a5177b70cd625579e7b2eb09be22b65c1262
caapap <37110214+caapap@users.noreply.github.com>
2023-11-29 17:56:35 +0800

    Update regex-pattern

--- scripts/export-images.py
+++ scripts/export-images.py
@@ -1,21 +1,28 @@
 # encoding: utf-8
+
 import re
 import os
 import subprocess
+
 if __name__ == "__main__":
     p = subprocess.Popen('docker images', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
 
-        # The regular expression here is to match the image name starting with hub.bk
-        # The actual use of the need to adjust their own
-        m = re.match(r'(^docker.io[^\s]*\s*)\s([^\s]*\s)', line)
+
+        m = re.match('('+ regex_pattern + r'[^\s]*\s*)\s([^\s]*\s)', line)
+        # 这一行正则匹配表达式，用来匹配含执行参数前缀的镜像文件
+        # 执行方法：python export-images.py <images-url-*>
+
+        # m = re.match(r'(^docker.io[^\s]*\s*)\s([^\s]*\s)', line)
+
         if not m:
             continue
-        # image name 
+
+        # 镜像名
         iname = m.group(1).strip()
         # tag
         itag = m.group(2).strip()
-        # tar name
+        # tar包的名字
         if iname.find('/'):
             tarname = iname.split('/')[0] + '_' + iname.split('/')[-1]  + '_' + itag + '.tar'
         else:
